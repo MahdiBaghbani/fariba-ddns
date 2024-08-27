5e7f3f874e29c10ac30b936ab4f8ecae6e7b496b
Mahdi Baghbani <mahdi-baghbani@azadehafzar.io>
2024-08-27 15:41:28 +0330

    remove: some stuff

--- src/models/config/resolvers/mod.rs
+++
@@ -1,9 +0,0 @@
-use serde::{Deserialize, Serialize};
-
-pub mod ident_me;
-
-#[derive(Serialize, Deserialize)]
-pub struct DnsResolvers {
-    #[serde(rename = "ident.me")]
-    pub ident_me: Option<ident_me::IdentMe>,
-}
